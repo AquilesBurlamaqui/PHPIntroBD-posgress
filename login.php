300178f252b5c2b535552dcb5d5dfb7e104de7b6
Aquiles Burlamaqui <aquilesburlamaqui@ect.ufrn.br>
2022-06-30 12:06:08 +0000

    Branch com os códigos referentes a Primeira Unidade

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
         <input type="text" name="email">
         Senha: 
         <input type="password" name="senha">
-        <input type="submit" value="Enviar">
+        <input type="submit" value="Logar">
     </form>
 
     <footer>
